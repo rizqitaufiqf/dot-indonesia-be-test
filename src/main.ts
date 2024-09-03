55a00769985af4542a06460edbb3d91097e885a0
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-09-03 13:17:49 +0700

    refactor: update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './config/config.type';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
+  app.enableVersioning({ type: VersioningType.URI });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const port = configService.getOrThrow('app.port', { infer: true });
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
